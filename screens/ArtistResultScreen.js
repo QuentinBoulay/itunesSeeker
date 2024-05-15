dfb186b9e1b4b503b384895f91a8f4a74a5ef6f4
QuentinBoulay <63411603+QuentinBoulay@users.noreply.github.com>
2024-05-15 16:15:57 +0200

    update pc (loader + home message)

--- screens/ArtistResultScreen.js
+++ screens/ArtistResultScreen.js
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ActivityIndicator } from 'react-native';
 import FlatListCustom from '../components/FlatListCustom';
 import Icon  from "react-native-vector-icons/AntDesign";
 
 const ArtistResult = ({ route, navigation }) => {
     const { item } = route.params;
     const [results, setResults] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         const artistName = item.artistName;
         const getTracks = async () => {
             const response = await fetch(`https://itunes.apple.com/search?term=${artistName}&entity=musicTrack`);
             const data = await response.json();
             console.log(data.results);
             setResults(data.results);
+            setLoading(false);
         }
 
         getTracks();
@@ -26,9 +29,11 @@ const ArtistResult = ({ route, navigation }) => {
                 <Text style={styles.title}>{item.artistName}</Text>
             </View>
             
-            <View style={{ marginTop: 100, borderTopWidth: 1, borderColor: "gray", paddingTop: 20}}>            
+            <View style={{ marginTop: 100, borderTopWidth: 1, borderColor: "gray", paddingTop: 20, flex: 1 }}>            
                 <Text style={[styles.subtitle, {marginBottom: 20}]}>Musique de l'artiste</Text>
-                <FlatListCustom data={results} category="musicTrack" navigation={navigation} />
+                {
+                    loading ? <View style={{justifyContent: 'center', flex: 1}}><ActivityIndicator size="large" color="black" /></View> : <FlatListCustom data={results} category="musicTrack" navigation={navigation} />
+                }
             </View>
 
         </View>
